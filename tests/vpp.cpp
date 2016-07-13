0563347d8459671e8bb9c6e1e364afb60963a26c
XuGuangxin <Guangxin.Xu@intel.com>
2016-07-13 15:45:12 +0800

    vpp: add denoise, sharpening and deinterlace
    add three options to yami vpp
        -s <level> optional, sharpening level
        --dn <level> optional, denoise level
        --di <mode>, optional, deinterlace mode, only support bob
    
    Signed-off-by: Zhong Xiaoxia <xiaoxia.zhong@intel.com>
    Signed-off-by: Xu Guangxin <guangxin.xu@intel.com>

--- tests/vpp.cpp
+++ tests/vpp.cpp
@@ -21,13 +21,18 @@
 #include "vppoutputencode.h"
 #include "encodeinput.h"
 #include "common/log.h"
-#include "VideoEncoderInterface.h"
-#include "VideoEncoderHost.h"
-#include "VideoPostProcessHost.h"
+#include <VideoEncoderInterface.h>
+#include <VideoEncoderHost.h>
+#include <VideoPostProcessHost.h>
+#include <YamiVersion.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
 
 using namespace YamiMediaCodec;
+
+void usage();
 
 SharedPtr<VppInput> createInput(const char* filename, const SharedPtr<VADisplay>& display)
 {
@@ -88,8 +93,18 @@ SharedPtr<FrameAllocator> createAllocator(const SharedPtr<VppOutput>& output, co
 class VppTest
 {
 public:
-    bool init(const char* input, const char* output)
+    VppTest()
+#if YAMI_CHECK_API_VERSION(0, 2, 1)
+        : m_sharpening(SHARPENING_LEVEL_NONE)
+        , m_denoise(DENOISE_LEVEL_NONE)
+        , m_deinterlaceMode(NULL)
+#endif
     {
+    }
+    bool init(int argc, char** argv)
+    {
+        if (!processCmdLine(argc, argv))
+            return false;
         m_display = createVADisplay();
         if (!m_display) {
             printf("create display failed");
@@ -99,8 +114,8 @@ public:
             ERROR("create vpp failed");
             return false;
         }
-        m_input = createInput(input, m_display);
-        m_output =  createOutput(output, m_display);
+        m_input = createInput(m_inputName, m_display);
+        m_output = createOutput(m_outputName, m_display);
         if (!m_input || !m_output) {
             printf("create input or output failed");
             return false;
@@ -133,19 +148,120 @@ public:
         return true;
     }
 private:
+    bool processCmdLine(int argc, char* argv[])
+    {
+        char opt;
+        const struct option long_opts[] = {
+            { "help", no_argument, NULL, 'h' },
+            { "sharpening", required_argument, NULL, 's' },
+            { "dn", required_argument, NULL, 0 },
+            { "di", required_argument, NULL, 0 },
+            { NULL, no_argument, NULL, 0 }
+        };
+        int option_index;
+
+        if (argc < 3) {
+            usage();
+            return false;
+        }
+
+        while ((opt = getopt_long_only(argc, argv, "s:h:", long_opts, &option_index)) != -1) {
+            switch (opt) {
+            case 'h':
+            case '?':
+                usage();
+                return false;
+            case 's':
+                m_sharpening = atoi(optarg);
+                break;
+            case 0:
+                switch (option_index) {
+                case 2:
+                    m_denoise = atoi(optarg);
+                    break;
+                case 3:
+                    m_deinterlaceMode = optarg;
+                    break;
+                default:
+                    usage();
+                    return false;
+                }
+                break;
+            default:
+                usage();
+                return false;
+            }
+        }
+        if (optind + 2 < argc) {
+            usage();
+            return false;
+        }
+        m_inputName = argv[optind++];
+        m_outputName = argv[optind++];
+        return true;
+    }
+
     bool createVpp()
     {
         NativeDisplay nativeDisplay;
         nativeDisplay.type = NATIVE_DISPLAY_VA;
         nativeDisplay.handle = (intptr_t)*m_display;
         m_vpp.reset(createVideoPostProcess(YAMI_VPP_SCALER), releaseVideoPostProcess);
-        return m_vpp->setNativeDisplay(nativeDisplay) == YAMI_SUCCESS;
+        if (m_vpp->setNativeDisplay(nativeDisplay) != YAMI_SUCCESS)
+            return false;
+#if YAMI_CHECK_API_VERSION(0, 2, 1)
+        {
+            VPPDenoiseParameters denoise;
+            memset(&denoise, 0, sizeof(denoise));
+            denoise.size = sizeof(denoise);
+            denoise.level = m_denoise;
+            if (m_vpp->setParameters(VppParamTypeDenoise, &denoise) != YAMI_SUCCESS) {
+                ERROR("denoise level should in range [%d, %d] or %d for none",
+                    DENOISE_LEVEL_MIN, DENOISE_LEVEL_MAX, DENOISE_LEVEL_NONE);
+                return false;
+            }
+        }
+
+        {
+            VPPSharpeningParameters sharpening;
+            memset(&sharpening, 0, sizeof(sharpening));
+            sharpening.size = sizeof(sharpening);
+            sharpening.level = m_sharpening;
+            if (m_vpp->setParameters(VppParamTypeSharpening, &sharpening) != YAMI_SUCCESS) {
+                ERROR("sharpening level should in range [%d, %d] or %d for none",
+                    SHARPENING_LEVEL_MIN, SHARPENING_LEVEL_MAX, SHARPENING_LEVEL_NONE);
+                return false;
+            }
+        }
+        if (m_deinterlaceMode) {
+            VPPDeinterlaceParameters deinterlace;
+            memset(&deinterlace, 0, sizeof(deinterlace));
+            deinterlace.size = sizeof(deinterlace);
+            if (strcasecmp(m_deinterlaceMode, "bob") == 0) {
+                deinterlace.mode = DEINTERLACE_MODE_BOB;
+            }
+            else {
+                ERROR("wrong mode deinterlace mode %s", m_deinterlaceMode);
+                return false;
+            }
+            if (m_vpp->setParameters(VppParamTypeDeinterlace, &deinterlace) != YAMI_SUCCESS) {
+                ERROR("deinterlace failed for mode %s", m_deinterlaceMode);
+                return false;
+            }
+        }
+#endif
+        return true;
     }
     SharedPtr<VADisplay> m_display;
     SharedPtr<VppInput> m_input;
     SharedPtr<VppOutput> m_output;
     SharedPtr<FrameAllocator> m_allocator;
     SharedPtr<IVideoPostProcess> m_vpp;
+    int32_t m_sharpening;
+    int32_t m_denoise;
+    char* m_deinterlaceMode;
+    char* m_inputName;
+    char* m_outputName;
 };
 
 void usage()
@@ -153,19 +269,17 @@ void usage()
     printf("a tool to do video post process, support scaling and CSC\n");
     printf("we can guess size and color format from your file name\n");
     printf("current supported format are i420, yv12, nv12\n");
-    printf("usage: yamivpp input_1920x1080.i420 output_320x240.yv12\n");
-
+    printf("usage: yamivpp <option> input_1920x1080.i420 output_320x240.yv12\n");
+    printf("       -s <level> optional, sharpening level\n");
+    printf("       --dn <level> optional, denoise level\n");
+    printf("       --di <mode>, optional, deinterlace mode, only support bob\n");
 }
 
 int main(int argc, char** argv)
 {
-    if (argc != 3) {
-        usage();
-        return -1;
-    }
     VppTest vpp;
-    if (!vpp.init(argv[1], argv[2])) {
-        ERROR("init vpp with %s, %s, failed", argv[1], argv[2]);
+    if (!vpp.init(argc, argv)) {
+        ERROR("init vpp failed");
         return -1;
     }
     if (!vpp.run()){
